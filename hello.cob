2d6e730e904b2a053e6ef090ca30f15e6e97b991
NCommander <ncommander@restless.systems>
2023-06-17 10:43:09 -0400

    Initial commit
    Signed-off-by: NCommander <ncommander@restless.systems>

---
+++ hello.cob
@@ -0,0 +1,8 @@
+OCOBOL >>SOURCE FORMAT IS FIXED
+       identification division.
+       program-id. hello.
+
+       data division.
+       procedure division.
+       display "Hello World".
+       stop run.
